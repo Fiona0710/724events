26424435ebd458ea4a6f096cf2c5ef971d6733c9
Fiona0710 <berthou.fiona@hotmail.fr>
2023-08-28 19:23:56 +0200

    -Creation of a state to store the last event.  -Call to the 'loadData' method of the api object to load the data - Calculation of the last event by sorting the events by date and retrieving the first element of the sorted list (which is the last event). -Using the context to provide the last event data (lastEvent) to the child components via the value property.

--- src/contexts/DataContext/index.js
+++ src/contexts/DataContext/index.js
@@ -19,9 +19,24 @@ export const api = {
 export const DataProvider = ({ children }) => {
   const [error, setError] = useState(null);
   const [data, setData] = useState(null);
+  // Création d'un état pour stocker le dernier événement.
+  const [lastEvent, setlastEvent] = useState(null);
+
   const getData = useCallback(async () => {
     try {
-      setData(await api.loadData());
+      // Appel à la méthode loadData de l'objet api pour charger les données
+      const apiData= await api.loadData();
+
+      // Stockage des données
+      setData(apiData);
+
+      // Calcul du dernier événement en triant les événements par date 
+      // et en récupérant le premier élément de la liste triée (qui est le dernier événement).
+      const last = apiData?.events.sort((evtA, evtB) =>
+        new Date(evtB.date) < new Date(evtA.date)? -1 : 1)[0];
+
+      // Stockage des données
+      setlastEvent(last)
     } catch (err) {
       setError(err);
     }
@@ -37,6 +52,9 @@ export const DataProvider = ({ children }) => {
       value={{
         data,
         error,
+        lastEvent,
+        // Utilisation du contexte pour fournir les données
+        //  le dernier événement (lastEvent) aux composants enfants via la propriété value.
       }}
     >
       {children}
