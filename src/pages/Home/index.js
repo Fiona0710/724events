c99b868c69cc325e3e25dfec9cf702a1abe5e22a
Fiona0710 <berthou.fiona@hotmail.fr>
2023-08-28 16:49:10 +0200

    -Added id for header nav anchors. -Added a .sort that sorts events in descending order according to their date, the latest (most recent) event will be first in the sorted array. - Added one that checks if 'last' exists and 'last.cover' and 'last.title' are defined otherwise returns null.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -13,7 +13,13 @@ import Modal from "../../containers/Modal";
 import { useData } from "../../contexts/DataContext";
 
 const Page = () => {
-  const {last} = useData()
+  const { data } = useData();
+  // On trie les événements dans l'ordre décroissant en fonction de leur date
+// Le dernier événement (le plus récent) sera en premier dans le tableau trié
+  const last = data?.events.sort((evtA, evtB) =>
+    new Date(evtB.date) < new Date(evtA.date)? -1 : 1
+  )[0];
+
   return <>
     <header>
       <Menu />
@@ -23,7 +29,7 @@ const Page = () => {
         <Slider />
       </section>
       <section className="ServicesContainer">
-        <h2 className="Title">Nos services</h2>
+        <h2 id="nos-services" className="Title">Nos services</h2>
         <p>Nous organisons des événements sur mesure partout dans le monde</p>
         <div className="ListContainer">
           <ServiceCard imageSrc="/images/priscilla-du-preez-Q7wGvnbuwj0-unsplash1.png">
@@ -52,11 +58,11 @@ const Page = () => {
         </div>
       </section>
       <section className="EventsContainer">
-        <h2 className="Title">Nos réalisations</h2>
+        <h2 id="nos-realisations" className="Title">Nos réalisations</h2>
         <EventList />
       </section>
       <section className="PeoplesContainer">
-        <h2 className="Title">Notre équipe</h2>
+        <h2 id="notre-equipe" className="Title">Notre équipe</h2>
         <p>Une équipe d’experts dédiés à l’ogranisation de vos événements</p>
         <div className="ListContainer">
           <PeopleCard
@@ -116,6 +122,8 @@ const Page = () => {
     <footer className="row">
       <div className="col presta">
         <h3>Notre derniére prestation</h3>
+         {/* Vérifie si 'last' existe et que 'last.cover' et 'last.title' sont définis */}
+        {last && last.cover && last.title ? (
         <EventCard
           imageSrc={last?.cover}
           title={last?.title}
@@ -123,6 +131,8 @@ const Page = () => {
           small
           label="boom"
         />
+        // Si l'une des conditions précédentes n'est pas satisfaite, ne rend rien (null)
+        ) : null}
       </div>
       <div className="col contact">
         <h3>Contactez-nous</h3>
