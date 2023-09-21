98a05519e5d71fc7a2c6aa636a4c4d5560dfec16
Fiona0710 <berthou.fiona@hotmail.fr>
2023-09-21 14:20:46 +0200

    Replacing .toEqual with .toBe which is more relevant here

--- src/helpers/Date/index.test.js
+++ src/helpers/Date/index.test.js
@@ -5,12 +5,12 @@ describe("Date helper", () => {
     describe("When getMonth is called", () => {
         it("the function return janvier for 2022-01-01 as date", () => {
             const result = getMonth(new Date('2022-01-01')) 
-            expect(result).toEqual('janvier');
+            expect(result).toBe('janvier');
         });
 
         it("the function return juillet for 2022-07-08 as date", () => {
             const result = getMonth(new Date('2022-07-08'))
-            expect(result).toEqual('juillet');
+            expect(result).toBe('juillet');
         });
     });
 })
